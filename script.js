76b2e781bbad99e86ebc469a74735f7776b38b6a
Namita Sajai <sajai.namita@gmail.com>
2023-05-18 00:15:46 -0400

    circle dot

--- script.js
+++ script.js
@@ -11,14 +11,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
   logo.addEventListener("mouseenter", function () {
     const timestamp = new Date().getTime();
-    logo.style.backgroundImage = `url(styles/RatLogo.gif?${timestamp})`;
-    // Force a reflow to restart the animation
-    void logo.offsetWidth;
+    logo.style.backgroundImage = `url(images/RatLogo.gif?${timestamp})`;
   });
 
   logo.addEventListener("mouseleave", function () {
-    logo.style.backgroundImage = "url(styles/RatLogo.png)";
+    logo.style.backgroundImage = "url(images/RatLogo.png)";
   });
+const pixelflower = document.getElementById('pixelflower');
+const circlePopup = document.getElementById('circle-popup');
+
+// Show the circle popup on mouseover pixelflower
+pixelflower.addEventListener('mouseover', () => {
+  circlePopup.style.animation = 'grow 2s 1';
+  circlePopup.style.display = 'block';
 });
 
+// Hide the circle popup on mouseout pixelflower
+pixelflower.addEventListener('mouseout', () => {
+  circlePopup.style.animation = 'grow 1s reverse';
+  circlePopup.style.display = 'none';
+});
+
+// Move the image within the circle on mousemove
+pixelflower.addEventListener('mousemove', (event) => {
+  // Calculate the mouse position relative to the pixelflower element
+  const mouseX = event.clientX - pixelflower.offsetLeft;
+  const mouseY = event.clientY - pixelflower.offsetTop;
+
+  // Calculate the parallax movement for the background image
+  const parallaxX = mouseX * 0.1 - 100; // Adjust the parallax speed as needed
+  const parallaxY = mouseY * 0.1 - 100; // Adjust the parallax speed as needed
+
+  // Apply the parallax movement to the background image
+  circlePopup.style.backgroundPositionX = `${parallaxX}px`;
+  circlePopup.style.backgroundPositionY = `${parallaxY}px`;
   
+});
+
+});
+
